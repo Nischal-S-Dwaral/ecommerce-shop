608ad4319011f0c3c798e68e10469ff60026f75d
Nischal-S-Dwaral <nischal.dwaral@gmail.com>
2023-03-12 20:59:30 +0000

    Get the users for the admin

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5001/api/";
-const TOKEN = () => {
-    if (JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser?.accessToken) {
-        return JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser?.accessToken
-    } else { return '' }
-};
+const TOKEN = (JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken)
+    ? JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser?.accessToken : '';
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL

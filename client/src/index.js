59978af08eebae20b4e6bba7d3d0191c19d5c4de
Nischal-S-Dwaral <nischal.dwaral@gmail.com>
2023-03-12 18:57:27 +0000

    Using React-persist to save user from re-login

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import {persistor, store} from './redux/store';
+import {PersistGate} from "redux-persist/integration/react";
 
+// PersistGate - This delays the rendering of your app's UI until your
+// persisted state has been retrieved and saved to redux.
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+      <PersistGate loading={null} persistor={persistor}>
+          <App />
+      </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
